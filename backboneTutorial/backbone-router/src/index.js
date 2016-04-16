1a39e7d4bef9764c6c106f3301c35283665717be
Ivan <wcy5211678@163.com>
2016-04-16 18:47:03 +0800

    router splitting

--- backboneTutorial/backbone-router/src/index.js
+++ backboneTutorial/backbone-router/src/index.js
@@ -1,22 +1,45 @@
-import $ from "jquery";
-import backbone from "backbone";
-
-$(".container").html("Hello backbone!");
-var Route = Backbone.Router.extend({
+import Backbone from "backbone";
+import home from './router/home.js';
+var HomeRoute = Backbone.Router.extend({
   routes: {
       '': 'home',
-      'about': 'about',
-      'pay': 'pay'
   },
-    home: function() {
-        $('.container').html('it is home');
-    },
-    about: function() {
-        $('.container').html('it is about');
-    },
-    pay: function() {
-        $('.container').html('it is pay');
-    }
+  home
 });
-new Route();
+new HomeRoute();
+
+// 延迟加载模块
+require.ensure([], function(require) {
+  var about = require('./router/pay.js').default;
+  var PayRoute = Backbone.Router.extend({
+    routes: {
+        'pay': 'pay'
+    },
+    about
+  });
+  new PayRoute();
+});
+require.ensure([], function(require) {
+  var about = require('./router/about.js').default;
+  var AboutRoute = Backbone.Router.extend({
+    routes: {
+        'about': 'about',
+    },
+    about
+  });
+  new AboutRoute();
+});
+// import pay from './router/pay.js';
+// import about from './router/about.js';
+
+// var HomeRoute = Backbone.Router.extend({
+//   routes: {
+//       '': 'home',
+//       // 'about': 'about',
+//       // 'pay': 'pay'
+//   },
+//     home,
+//     // about: about,
+//     // pay: pay,
+// });
 Backbone.history.start();
---
+++ backboneTutorial/backbone-router/src/router/pay.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+export default () => {
+  $('.container').html('it is pay');
+};
