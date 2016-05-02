482e3cb93596175800cc3989943226342705219e
Ivan <wcy5211678@163.com>
2016-05-02 17:51:59 +0800

    webpack config add comment

--- webpackTutorial/UsingReactWithWebpackTutorial/webpack.config.js
+++ webpackTutorial/UsingReactWithWebpackTutorial/webpack.config.js
@@ -1,19 +1,18 @@
-console.log(__dirname);
 import path from 'path';
-console.log(__dirname);
+
 const config = {
-  devtool: 'eval-source-map',
+  devtool: 'eval-source-map', // 开发工具，生成 source-map 可以还原源代码用于调试
   entry: path.join(__dirname, 'app/main.js'),
   output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
+    path: path.join(__dirname, '/dist/'), // 输出到指定文件夹
+    filename: '[name].js', // 原名输出
     publicPath: '/'
   },
   module: {
     loaders: [{
       test: /\.js?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel' // babel-loader babel ES6 加载器
     }]
   }
 };
