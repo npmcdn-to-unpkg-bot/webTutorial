947402de589c6533cb5ef96a0f0d7ed86916719f
Ivan <wcy5211678@163.com>
2016-08-30 15:07:05 +0800

    chat example

--- socketIO/chat-example/index.js
+++ socketIO/chat-example/index.js
@@ -16,6 +16,10 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   })
 })
 
